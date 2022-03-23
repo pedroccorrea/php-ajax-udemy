473f7b371355635129f2c23ba6c9e8b4f358648d
Pedro  C. Correa <98551189+pedroccorrea@users.noreply.github.com>
2022-03-23 12:46:18 -0300

    Update exercicio.php

--- unidade_04/exercicio.php
+++ unidade_04/exercicio.php
@@ -1,16 +1,34 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>PHP com AJAX</title>
-    </head>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP com AJAX</title>
+</head>
+<body>
+    <div id="listagem"></div>
 
-    <body>
-        <div id="listagem"></div>
-        
-        <script src="jquery.js"></script>
-        <script>
+    <script src="jquery.js"></script>
+    <script>
+        $.ajax({
+            url:'_xml/produtos.xml'
+        }).then(sucesso, falha);
 
-        </script>
-    </body>
+        function sucesso(arquivo) {
+            var elemento;
+            elemento ="<ul>";
+            $(arquivo).find('produto').each(function() {
+                var nome = $(this).find('nomeproduto').text();
+                elemento += "<li>" + nome + "</li>";
+            })
+            elemento += "</ul>"
+            $('div#listagem').html(elemento);
+        }
+
+        function falha() {
+
+        }
+    </script>
+</body>
 </html>
