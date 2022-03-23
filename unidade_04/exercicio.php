20e87e3bbb4757532fdfda240e80f64ce095757d
Pedro  C. Correa <98551189+pedroccorrea@users.noreply.github.com>
2022-03-23 16:51:53 -0300

    update

--- unidade_04/exercicio.php
+++ unidade_04/exercicio.php
@@ -5,22 +5,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP com AJAX</title>
+    <link rel="stylesheet" href="_css/style.css">
 </head>
 <body>
+    <button id="botao">Carregar</button>
     <div id="listagem"></div>
 
     <script src="jquery.js"></script>
     <script>
-        $.ajax({
-            url:'_xml/produtos.xml'
-        }).then(sucesso, falha);
+        $('button#botao').click(function() {
+            $('div#listagem').css('display', 'block');
+            carregarDados();
+        });
 
-        function sucesso(arquivo) {
-            console.log(arquivo);
-        }
+        function carregarDados() {
+            $.ajax({
+                url:'_xml/produtos.xml'
+            }).then(sucesso,falha);
 
-        function falha() {
-            console.log("Falhou");
+            function sucesso(arquivo) {
+                var elemento;
+                elemento = "<ul>";
+                $(arquivo).find('produto').each(function () {
+                    var nome = $(this).find('nomeproduto').text();
+                    var preco = $(this).find('precounitario').text();
+                    elemento += "<li class='nome'>" + nome + "</li>";
+                    elemento += "<li class='preco'>" + "R$" + preco + "</li>"
+                });
+
+                elemento += "</ul>";
+                $('div#listagem').html(elemento);
+
+            }
+
+            function falha() {
+                $('div#listagem').html("Falha ao carregar dados");
+            }
         }
     </script>
 </body>
--- unidade_05/exercicio.php
+++ unidade_05/exercicio.php
@@ -1,15 +1,35 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>PHP com AJAX</title>
-    </head>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP com AJAX</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <button id="botao">Carregar</button>
+    <div id="listagem"></div>
 
-    <body>
-        <div id="listagem"></div>
-        <script src="jquery.js"></script>
-        <script>
+    <script src="jquery.js"></script>
+    <script>
+        $('button#botao').click(function() {
+            carregarDados();
+            $('div#listagem').css('display', 'block');
+        })
+        function carregarDados(){
 
-        </script>
-    </body>
+            $.getJSON('_json/produtos.json', function(data) {
+                var elemento;
+                elemento = "<ul>";
+                $.each(data, function(i, valor) {
+                    elemento += "<li class='nome'>" + valor.nomeproduto + "</li>";
+                    elemento += "<li class='preco'>" + valor.precounitario + "</li>";
+                })
+                elemento += "</ul";
+                $('div#listagem').html(elemento);
+            });
+        }
+    </script>
+</body>
 </html>
