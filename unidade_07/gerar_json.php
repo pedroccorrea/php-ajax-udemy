ae413c8381690bb423577965334a78dda3f3af76
Pedro  C. Correa <98551189+pedroccorrea@users.noreply.github.com>
2022-03-24 08:22:53 -0300

    update
    Unidade 07

--- unidade_07/gerar_json.php
+++ unidade_07/gerar_json.php
@@ -1,17 +1,30 @@
 <?php
-    // abrir conexao
-    $conecta = mysqli_connect("localhost","root","","andes");
+    //preparar o arquivo para callback
+    $callback = isset($_GET['callback']) ? $_GET['callback'] : false;
 
-    $selecao = "SELECT nomeproduto, precounitario, imagempequena FROM produtos";
-    $produtos = mysqli_query($conecta,$selecao);
+    // abrir conexão
+    $conectar = mysqli_connect("localhost", "root", "", "andes");
+
+    // realizar consulta ao banco de dados
+    $produtos = "SELECT
+                    nomeproduto, precounitario, imagempequena
+                FROM
+                    produtos";
+    $con_produtos = mysqli_query($conectar, $produtos);
+    if(!$con_produtos) {
+        die("Erro ao conectar com o banco de dados.");
+    }
 
     $retorno = array();
-    while($linha = mysqli_fetch_object($produtos)) {
-        $retorno[] = $linha;
-    }   
 
-    echo json_encode($retorno);
-    
-    // fechar conecta
-    mysqli_close($conecta);
+    // criar json
+    while($linha = mysqli_fetch_object($con_produtos)) {
+        $retorno[] = $linha;
+    }
+    echo ($callback? $callback . '(': '') . json_encode($retorno) . ($callback? ')' : '');
+?>
+
+<?php
+    // fechar conexão
+    mysqli_close($conectar);
 ?>
--- unidade_07/listagem.php
+++ unidade_07/listagem.php
@@ -1,12 +1,17 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>PHP com AJAX</title>
-     
-    </head>
-
-    <body>
-
-    </body>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP com AJAX</title>
+    <script>
+        function retornarProdutos(data) {
+            console.log(data[0].nomeproduto);
+        }
+    </script>
+</head>
+<body>
+    <script src="http://localhost/php-ajax-udemy/php-ajax-udemy/unidade_07/gerar_json.php?callback=retornarProdutos"></script>
+</body>
 </html>
